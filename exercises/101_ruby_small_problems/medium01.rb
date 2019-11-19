e6e37c5de919efc981903c208ab6ca9bfec4734a
Andrew Ratliff <andrew.ratliff@gmail.com>
2019-11-19 14:33:33 +0700

    ruby small problems medium01 exercises

--- exercises/101_ruby_small_problems/medium01.rb
+++ exercises/101_ruby_small_problems/medium01.rb
@@ -36,23 +36,18 @@ end
 # Rotation (Part 3)
 
 def max_rotation(num)
-  (2..(num.size)).each do |digit|
-    result = rotate_rightmost_digits(num, -digit)
+  num.to_s.size.downto(2).each do |n|
+    num = rotate_rightmost_digits(num, n)
   end
 
-  result
+  num
 end
 
-#puts max_rotation(735291) == 321579
-
-## num = 735291
-##
-
-
-#puts max_rotation(3) == 3
-#puts max_rotation(35) == 53
-#puts max_rotation(105) == 15 # the leading zero gets dropped
-#puts max_rotation(8_703_529_146) == 7_321_609_845
+# puts max_rotation(735291) == 321579
+# puts max_rotation(3) == 3
+# puts max_rotation(35) == 53
+# puts max_rotation(105) == 15 # the leading zero gets dropped
+# puts max_rotation(8_703_529_146) == 7_321_609_845
 
 # 1000 Lights
 
@@ -79,27 +74,96 @@ end
 
 # Diamonds!
 
-def print
-end
-
-def diamond(size)
+def diamond(odd_num)
   counter = 1
 
-  while counter <= num
-    puts ("*" * counter).center(num)
+  while counter <= odd_num
+    if counter > 1
+      puts ("*" + (" " * (counter - 2)) + "*").center(odd_num)
+    else
+      puts "*".center(odd_num)
+    end
+    # puts ("*" * counter).center(odd_num)
     counter += 2
   end
 
-  counter = num
+  counter = odd_num - 2
 
   while counter >= 0
+    if counter > 1
+      puts ("*" + (" " * (counter - 2)) + "*").center(odd_num)
+    else
+      puts "*".center(odd_num)
+    end
+    # puts ("*" * counter).center(odd_num)
     counter -= 2
-    puts ("*" * counter).center(num)
   end
 end
 
 # diamond(1)
 # diamond(9)
+
+# Stack Machine Interpretation
+
+def minilang(input)
+  stack = []
+  register = 0
+
+  input.split.each do |item|
+    case item
+    when "PRINT"
+      puts register
+    when "PUSH"
+      stack << register
+    when "MULT"
+      register *= stack.pop
+    when "ADD"
+      register += stack.pop
+    when "POP"
+      register = stack.pop
+    when "SUB"
+      register -= stack.pop
+    when "DIV"
+      register /= stack.pop
+    when "MOD"
+      register %= stack.pop
+    else
+      register = item.to_i
+    end
+  end
+end
+
+# minilang('PRINT')
+# 0
+
+# minilang('5 PUSH 3 MULT PRINT')
+# 15
+
+# minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
+# 5
+# 3
+# 8
+
+# minilang('5 PUSH POP PRINT')
+# 5
+
+# minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
+# 5
+# 10
+# 4
+# 7
+
+# minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
+# 6
+
+# minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
+# 12
+
+# minilang('-3 PUSH 5 SUB PRINT')
+# 8
+
+# minilang('6 PUSH')
+# (nothing printed; no PRINT commands)
 
 # Word to Digit
 
@@ -118,7 +182,6 @@ WORDS_TO_NUMBERS = {
 
 def word_to_digit(string)
   string.split.map do |word|
-    binding.pry if word == "four."
     if WORDS_TO_NUMBERS.keys.include?(word)
       word.gsub(/#{word}/, WORDS_TO_NUMBERS[word].to_s)
     else
@@ -127,10 +190,35 @@ def word_to_digit(string)
   end.join(" ")
 end
 
+# LS solution
+def word_to_digit(words)
+  WORDS_TO_NUMBERS.keys.each do |word|
+    words.gsub!(/\b#{word}\b/, WORDS_TO_NUMBERS[word].to_s)
+  end
+
+  words
+end
+
 # puts word_to_digit('Please call me at five five five one two three four. Thanks.') # == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
 
 # Fibonacci Numbers (Recursion)
 
-def fib(n, start=1)
+def fib(num, sum = 0, count = 1)
+  puts "***"
+  puts "num: #{num}"
+  puts "sum: #{sum}"
+  puts "count: #{count}"
+  if sum.zero?
+    sum += count
+  else
+    sum += sum
+  end
 
+  return sum if count == num
+
+  count += 1
+  fib(num, sum, count)
 end
+
+# puts fib(1)
+puts fib(3)
