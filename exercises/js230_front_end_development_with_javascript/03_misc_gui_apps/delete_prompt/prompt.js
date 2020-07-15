92be406364bdfaeaf51118320b9e0f9b652912b9
Andrew Ratliff <andrew.ratliff@gmail.com>
2020-07-16 00:17:22 +0700

    finish confirm prompt exercise

--- exercises/js230_front_end_development_with_javascript/03_misc_gui_apps/delete_prompt/prompt.js
+++ exercises/js230_front_end_development_with_javascript/03_misc_gui_apps/delete_prompt/prompt.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const confirmTemplateSource = document.getElementById("confirm_template");
   const todoTemplate = Handlebars.compile(todoTemplateSource.innerHTML);
   const confirmTemplate = Handlebars.compile(confirmTemplateSource.innerHTML);
+  const confirmPrompt = document.querySelector(".confirm_prompt");
+  const overlay = document.querySelector(".overlay");
   const todo_items = [
     { id: 1, title: 'Homework' },
     { id: 2, title: 'Shopping' },
@@ -16,6 +18,31 @@ document.addEventListener('DOMContentLoaded', () => {
   ul.addEventListener("click", event => {
     if (event.target instanceof HTMLSpanElement) {
       const id = event.target.parentElement.dataset.id;
+      const todo = todo_items.find(todo => todo.id === id);
+
+      overlay.style.display = "block";
+      confirmPrompt.style.display = "block";
+      confirmPrompt.innerHTML = confirmTemplate(todo);
+
+      const yes = document.querySelector(".confirm_yes");
+      yes.addEventListener('click', event => {
+        event.preventDefault();
+        const li = document.querySelector(`li[data-id='${id}']`);
+        li.remove();
+        resetOverlay();
+      });
+
+      const no = document.querySelector(".confirm_no");
+      no.addEventListener('click', event => {
+        event.preventDefault();
+        resetOverlay();
+      });
     }
   })
+
+  function resetOverlay() {
+    overlay.style.display = 'none';
+    confirmPrompt.style.display = 'none';
+    confirmPrompt.innerHTML = '';
+  }
 });
