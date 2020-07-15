0e0cde48efcb3a81fa89d3cefa90721e153061fe
Andrew Ratliff <andrew.ratliff@gmail.com>
2020-07-15 23:53:34 +0700

    confirm prompt wip

--- exercises/js230_front_end_development_with_javascript/03_misc_gui_apps/delete_prompt/prompt.js
+++ exercises/js230_front_end_development_with_javascript/03_misc_gui_apps/delete_prompt/prompt.js
@@ -1,1 +1,21 @@
-console.log('hi');
+document.addEventListener('DOMContentLoaded', () => {
+  const ul = document.querySelector("#todos");
+  const todoTemplateSource = document.getElementById("todos_template");
+  const confirmTemplateSource = document.getElementById("confirm_template");
+  const todoTemplate = Handlebars.compile(todoTemplateSource.innerHTML);
+  const confirmTemplate = Handlebars.compile(confirmTemplateSource.innerHTML);
+  const todo_items = [
+    { id: 1, title: 'Homework' },
+    { id: 2, title: 'Shopping' },
+    { id: 3, title: 'Calling Mom' },
+    { id: 4, title: 'Coffee with John '}
+  ];
+
+  ul.innerHTML = todoTemplate({todos: todo_items});
+
+  ul.addEventListener("click", event => {
+    if (event.target instanceof HTMLSpanElement) {
+      const id = event.target.parentElement.dataset.id;
+    }
+  })
+});
